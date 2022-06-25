0ad405fe16823632354f66a626c12f769bfd6cea
Benjamin Redd <bentheredd13@gmail.com>
2022-06-24 19:28:51 -0700

    Add additional clarifying comments

--- pw-loop.sh
+++ pw-loop.sh
@@ -1,11 +1,12 @@
 #!/usr/local/bin/zsh
 
-#This script will loop over a playwright test the specified number of times
+#This script will loop over a playwright test the specified number of times. It will only work properly with pre-set playwright scripts that you've configured in a local project.
 
-NUM=$1
-TEST=$2
-ENV=$3
-OPTIONS=$4
+#This script takes 4 arguments from a user. e.g. "pw-loop 5 conditions.spec.mjs qa --headed"
+NUM=$1 #the number of times to loop over a playwright test
+TEST=$2 #the test name to loop over
+ENV=$3 #the environment to run the test against. Local by default, or QA url if specified by the user
+OPTIONS=$4 #any playwright option flags, e.g. --headed
 
 if [[ $ENV == "qa" ]]; 
 then
